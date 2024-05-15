0928677a3545a073977f10eefef52d3f247b3e01
minhtamphan373 <128771298+minhtamphan373@users.noreply.github.com>
2024-05-15 20:10:21 +0700

    Update memberlist.php [CSM-65][Admin] Delete Customer Account

--- admin/memberlist.php
+++ admin/memberlist.php
@@ -7,45 +7,62 @@ include "leftside.php";
 $comment = new comment;
 $show_member = $comment -> show_member()
 ?>
-       <div class="admin-content-right">
-            <div class="table-content">
-                <table>
-                    <tr>
-                        <th>Stt</th>
-                        <th>ID</th>
-                        <th>Username</th>
-                        <th>Password</th>
-                        <th>Email</th>
-                        <th>Status</th>
-                        <th>Tùy chỉnh</th>
-                    </tr>
-                    <?php
+<div class="admin-content-right">
+    <div class="table-content">
+        <table>
+            <tr>
+                <th>Stt</th>
+                <th>ID</th>
+                <th>Username</th>
+                <th>Password</th>
+                <th>Email</th>
+                <th>Status</th>
+                <th>Tùy chỉnh</th>
+            </tr>
+            <?php
                     if($show_member){$i=0; while($result= $show_member->fetch_assoc()){
                         $i++
                    
                     ?>
-                    <tr>
-                        <td> <?php echo $i ?></td>
-                        <td> <?php echo $result['user_id'] ?></td>
-                        <td> <a href="memberinfo.php?user_id=<?php echo $result['user_id'] ?>"><?php echo $result['user_ten']  ?></a></td>
-                        <td> <?php echo $result['user_password']  ?></td>
-                        <td> <?php echo $result['user_email']  ?></td>
-                        <td> <?php echo $result['status'] ?></td?>
-                        <td>
-                            <a href="memberdelete.php?user_id=<?php echo $result['user_id'] ?>">Lock</a> |
-                            <a href="memberdelete.php?user_id=<?php echo $result['user_id'] ?>">Xóa</a>
-                        </td>
-                    </tr>
+            <tr>
+                <td> <?php echo $i ?></td>
+                <td> <?php echo $result['user_id'] ?></td>
+                <td> <a
+                        href="memberinfo.php?user_id=<?php echo $result['user_id'] ?>"><?php echo $result['user_ten']  ?></a>
+                </td>
+                <td> <?php echo $result['user_password']  ?></td>
+                <td> <?php echo $result['user_email']  ?></td>
+                <td>
+                    <?php 
+                    if($result['status'] == 1){
+                        echo '<p><a href="memberstatus.php?user_id='.$result['user_id'].'&status=0" 
+                        class="btn btn-success">Active</a></p>';
+                    } else {
+                        echo '<p><a href="memberstatus.php?user_id='.$result['user_id'].'&status=1" 
+                        class="btn btn-danger">Inactive</a></p>';
+                    }
+                    ?>
+
+
+                    </td?>
+                <td>
                     <?php
+                    
+                    ?>
+                    <a href="memberdelete.php?user_id=<?php echo $result['user_id'] ?>">Xóa</a>
+                </td>
+            </tr>
+            <?php
                      }}
                     ?>
-                   
-                </table>
-               </div>        
-        </div>
-    </section>
-    <section>
-    </section>
-    <script src="js/script.js"></script>
+
+        </table>
+    </div>
+</div>
+</section>
+<section>
+</section>
+<script src="js/script.js"></script>
 </body>
-</html>  
+
+</html>
